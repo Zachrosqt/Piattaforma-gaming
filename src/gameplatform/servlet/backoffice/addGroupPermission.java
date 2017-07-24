60646788647a1766722df06e4d8513ffb4a2b607
pio-9 <pio-9@DESKTOP-DO1VFR1>
2017-07-24 13:20:56 +0200

    Commit

--- src/gameplatform/servlet/backoffice/addGroupPermission.java
+++ src/gameplatform/servlet/backoffice/addGroupPermission.java
@@ -84,7 +84,6 @@ import gameplatform.db.*;
 		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 		 */
 		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			// TODO Auto-generated method stub
 			
 			Configuration conf = new Configuration().configure();
 			Session session = conf.buildSessionFactory().getCurrentSession();
